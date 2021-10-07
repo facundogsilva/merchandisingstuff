23aa84ffae270952500c7a35565e5a68f8e8398c
facundogsilva <88751546+facundogsilva@users.noreply.github.com>
2021-10-07 12:47:03 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -16,6 +16,59 @@ http://api.argentina.cdo.dev.yellowspot.com.ar/v1/products?auth_token=Ny8WOEdrmC
 
 
 var APIGenericProduct;
+var productContainer = document.getElementById("product-container"); 
+function printProduct(product) {
+  var productTitle = `<h4> ${product.name} </h4>`;
+  var productImage = `<img src="${product.images[0].image_url}" class="product-images-general">`
+  var productDiv = document.createElement("div");
+  productDiv.classList.add("single-product");
+  productDiv.innerHTML = productTitle + productImage;
+  productContainer.appendChild(productDiv);
+}
+
+
+(async function () {
+  const requestUrl = "https://api.zecatdifapro.com/generic_product";
+  let response = await fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => APIGenericProduct = data.generic_products);
+  APIGenericProduct.forEach((prod) => console.log(prod));
+  APIGenericProduct.forEach((prod) => printProduct(prod));
+})();
+
+
+async function apiHandler(url, store) {
+  const requestUrl = "http://api.argentina.cdo.dev.yellowspot.com.ar/v1/products?auth_token=Ny8WOEdrmCwBJ3-U-oOYtg";
+  let response = await fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => store = data.generic_products);
+  store.forEach((prod) => console.log(prod));
+  store.forEach((prod) => printProduct(prod));
+}
+
+
+
+/*
+$.getJSON('api_example.json', function(data) {
+
+var salida = '';
+
+for( var i = 0; i < products.lenght; i++) {
+  salida += '<li>' products.name + '</li>';
+}
+
+document.getElementById('products')
+  .innerHTML = salida;
+}
+*/
+
+/* so DARK-SIDED */ 
+
+/* index */
+
+/* 
+
+var APIGenericProduct;
 
 (async function () {
   const requestUrl = "https://api.zecatdifapro.com/generic_product";
@@ -25,6 +78,4 @@ var APIGenericProduct;
   APIGenericProduct.forEach((prod) => console.log(prod))
 })();
 
-
-/* index */
-
+*/
